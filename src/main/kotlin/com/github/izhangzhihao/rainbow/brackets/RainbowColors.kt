986623a08cd2be68ebe06f082a97121e7e6e372c
Yii.Guxing <yii.guxing@gmail.com>
2018-01-10 17:35:16 +0800

    Add colors for default theme.

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RainbowColors.kt
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RainbowColors.kt
@@ -1,28 +1,30 @@
 package com.github.izhangzhihao.rainbow.brackets
 
+import com.intellij.ui.JBColor
 import java.awt.Color
+
 
 object RainbowColors {
 
-    val roundBracketsColor = arrayOf(
-            Color(230, 180, 34),
-            Color(0, 169, 96),
-            Color(252, 116, 130),
-            Color(156, 39, 176),
-            Color(33, 150, 243)
+    val roundBracketsColor: Array<Color> = arrayOf(
+            JBColor(0xE66A01, 0xE6B422),
+            JBColor(0x109321, 0x00A960),
+            JBColor(0xFF3E5D, 0xFC7482),
+            JBColor(0x9C27B0, 0x9C27B0),
+            JBColor(0x006BE7, 0x2196F3)
     )
 
-    val squareBracketsColor = arrayOf(
-            Color(51, 204, 255),
-            Color(128, 128, 255),
-            Color(0, 115, 168)
+    val squareBracketsColor: Array<Color> = arrayOf(
+            JBColor(0x0B9087, 0x33CCFF),
+            JBColor(0x6444E6, 0x8080FF),
+            JBColor(0x00599B, 0x0073A8)
     )
 
-    val squigglyBracketsColor = arrayOf(
-            Color(25, 118, 210),
-            Color(205, 220, 57),
-            Color(150, 80, 150),
-            Color(255, 152, 99)
+    val squigglyBracketsColor: Array<Color> = arrayOf(
+            JBColor(0x0057D2, 0x1976D2),
+            JBColor(0x509629, 0xCDDC39),
+            JBColor(0xA526AA, 0x965096),
+            JBColor(0xFF6D27, 0xFF9863)
     )
 
     val angleBracketsColor = roundBracketsColor
