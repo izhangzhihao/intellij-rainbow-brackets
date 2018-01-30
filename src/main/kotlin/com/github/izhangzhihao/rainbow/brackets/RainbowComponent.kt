6e947c94582101998324fd095f60f9df867a0474
Yii.Guxing <yii.guxing@gmail.com>
2018-01-30 14:15:17 +0800

    Enable highlight visitor

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RainbowComponent.kt
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RainbowComponent.kt
@@ -1,10 +1,9 @@
 package com.github.izhangzhihao.rainbow.brackets
 
-import com.github.izhangzhihao.rainbow.brackets.RainbowUtils.registerAnnotatorForAllLanguages
 import com.intellij.openapi.components.ApplicationComponent
 
 class RainbowComponent : ApplicationComponent {
     override fun initComponent() {
-        registerAnnotatorForAllLanguages()
+        BracePairs.init()
     }
 }
