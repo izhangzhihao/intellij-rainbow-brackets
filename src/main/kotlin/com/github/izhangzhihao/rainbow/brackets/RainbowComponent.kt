0d6e99820bb92354511daf98557e415e9bffd5c6
张志豪 <izhangzhihao@hotmail.com>
2018-02-20 11:37:51 +0800

    Remove unused `ApplicationComponent`

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RainbowComponent.kt
+++
@@ -1,8 +0,0 @@
-package com.github.izhangzhihao.rainbow.brackets
-
-import com.intellij.openapi.components.ApplicationComponent
-
-class RainbowComponent : ApplicationComponent {
-    override fun initComponent() {
-    }
-}
