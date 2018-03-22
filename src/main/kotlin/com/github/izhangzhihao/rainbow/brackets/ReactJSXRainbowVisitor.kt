e7a671f816d181c6fbe957c3c0098e75a0c09d86
张志豪 <izhangzhihao@hotmail.com>
2018-03-22 13:47:46 +0800

    Try fix compatibility problem with IC-distribution

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/ReactJSXRainbowVisitor.kt
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/ReactJSXRainbowVisitor.kt
@@ -4,13 +4,13 @@ import com.intellij.codeInsight.daemon.impl.HighlightVisitor
 import com.intellij.lang.javascript.DialectDetector
 import com.intellij.psi.PsiFile
 
-class ReactRainbowVisitor : XmlRainbowVisitor() {
+class ReactJSXRainbowVisitor : XmlRainbowVisitor() {
 
     override fun suitableForFile(file: PsiFile): Boolean {
         return DialectDetector.isJSX(file)
     }
 
     override fun clone(): HighlightVisitor {
-        return ReactRainbowVisitor()
+        return ReactJSXRainbowVisitor()
     }
 }
