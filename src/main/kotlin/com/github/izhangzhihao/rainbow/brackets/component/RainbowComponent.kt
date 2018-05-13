1ca66ad01a260e771e1a6534c6c8de4e509ca5f5
Yii.Guxing <yii.guxing@gmail.com>
2018-05-13 20:18:43 +0800

    Add action manager for editor event

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/component/RainbowComponent.kt
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/component/RainbowComponent.kt
@@ -1,5 +1,7 @@
 package com.github.izhangzhihao.rainbow.brackets.component
 
+import com.github.izhangzhihao.rainbow.brackets.CtrlHandler
+import com.github.izhangzhihao.rainbow.brackets.EditorEventActionManager
 import com.github.izhangzhihao.rainbow.brackets.settings.RainbowSettings
 import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.ide.plugins.PluginManager
@@ -14,20 +16,34 @@ class RainbowComponent : ApplicationComponent {
     var updated: Boolean = false
 
     override fun initComponent() {
+        EditorEventActionManager.instance.apply {
+            registerEditorEventListener(CtrlHandler())
+            install()
+        }
+
         updated = getPlugin()?.version != RainbowSettings.instance.version
         if (updated) {
             RainbowSettings.instance.version = getPlugin()?.version
         }
         if (!RainbowSettings.instance.isRainbowifyHTMLInsideJS) {
-            EditorColorsManager.getInstance().globalScheme.setAttributes(createTextAttributesKey("HTML_CODE"), TextAttributes())
+            EditorColorsManager
+                    .getInstance()
+                    .globalScheme
+                    .setAttributes(createTextAttributesKey("HTML_CODE"), TextAttributes())
             RainbowSettings.instance.isRainbowifyHTMLInsideJS = true
         }
+    }
+
+    override fun disposeComponent() {
+        EditorEventActionManager.instance.uninstall()
+        super.disposeComponent()
     }
 
     companion object {
         val instance: RainbowComponent
             get() = ApplicationManager.getApplication().getComponent(RainbowComponent::class.java)
 
-        private fun getPlugin(): IdeaPluginDescriptor? = PluginManager.getPlugin(PluginId.getId("izhangzhihao.rainbow.brackets"))
+        private fun getPlugin(): IdeaPluginDescriptor? = PluginManager.getPlugin(
+                PluginId.getId("izhangzhihao.rainbow.brackets"))
     }
 }
