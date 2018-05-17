c1b86a0b49d9f53ae98343177a44f18d996cf9d2
Yii.Guxing <yii.guxing@gmail.com>
2018-05-18 00:32:51 +0800

    Highlight current scope use shortcut. see: #61

---
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/action/ScopeOutsideHighlightingRestrainAction.kt
@@ -0,0 +1,55 @@
+package com.github.izhangzhihao.rainbow.brackets.action
+
+import com.github.izhangzhihao.rainbow.brackets.RainbowInfo
+import com.intellij.codeInsight.highlighting.HighlightManager
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.markup.EffectType
+import com.intellij.openapi.editor.markup.RangeHighlighter
+import com.intellij.openapi.editor.markup.TextAttributes
+import java.awt.Color
+import java.awt.Font
+import java.util.*
+
+/**
+ * ScopeOutsideHighlightingRestrainAction
+ *
+ * Created by Yii.Guxing on 2018/05/15
+ */
+class ScopeOutsideHighlightingRestrainAction : AbstractScopeHighlightingAction() {
+
+    override fun Editor.addHighlighter(highlightManager: HighlightManager,
+                                       rainbowInfo: RainbowInfo): Collection<RangeHighlighter> {
+        val defaultBackground = EditorColorsManager.getInstance().globalScheme.defaultBackground
+        val background = Color.GRAY.alphaBlend(defaultBackground, 0.06f)
+        val foreground = Color.GRAY.alphaBlend(defaultBackground, 0.55f)
+        val attributes = TextAttributes(foreground, background, background, EffectType.BOXED, Font.PLAIN)
+        val highlighters = LinkedList<RangeHighlighter>()
+
+        val startOffset = rainbowInfo.startOffset
+        if (startOffset > 0) {
+            highlightManager.addRangeHighlight(this,
+                    0,
+                    startOffset,
+                    attributes,
+                    true,
+                    true,
+                    highlighters)
+        }
+
+        val endOffset = rainbowInfo.endOffset
+        val lastOffset = document.textLength
+        if (endOffset < lastOffset) {
+            highlightManager.addRangeHighlight(this,
+                    endOffset,
+                    lastOffset,
+                    attributes,
+                    true,
+                    true,
+                    highlighters)
+        }
+
+        return highlighters
+    }
+
+}
