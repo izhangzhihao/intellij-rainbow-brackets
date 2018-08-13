2862a5ff60a3f530ccd0dbb88edef972ff031d26
张志豪 <izhangzhihao@hotmail.com>
2018-08-13 23:53:35 +0800

    Revert "Add version info & config file path in settings form" Cause `java.lang.NoSuchMethodError: com.intellij.openapi.components.impl.stores.IComponentStore.getStateStorageManager()Lcom/intellij/configurationStore/StateStorageManager;` on intellij 2018 and above
    This reverts commit 65cd2ebe3b6ce7ffecaf8fc1b2dd270fc8bef72a.

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/settings/form/RainbowSettingsForm.kt
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/settings/form/RainbowSettingsForm.kt
@@ -1,12 +1,7 @@
 package com.github.izhangzhihao.rainbow.brackets.settings.form
 
 import com.github.izhangzhihao.rainbow.brackets.settings.RainbowSettings
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.stateStore
-import com.intellij.util.io.systemIndependentPath
-import java.nio.file.Paths
 import javax.swing.*
-import javax.swing.border.EmptyBorder
 
 class RainbowSettingsForm {
     private var panel: JPanel? = null
@@ -17,8 +12,6 @@ class RainbowSettingsForm {
     private var enableRainbowSquareBrackets: JCheckBox? = null
     private var enableRainbowAngleBrackets: JCheckBox? = null
     private var doNOTRainbowifyBracketsWithoutContent: JCheckBox? = null
-    private var version: JLabel? = null
-    private var info: JTextArea? = null
 
     private val settings: RainbowSettings = RainbowSettings.instance
 
@@ -55,21 +48,5 @@ class RainbowSettingsForm {
         enableRainbowSquigglyBrackets?.isSelected = settings.isEnableRainbowSquigglyBrackets
         enableRainbowSquareBrackets?.isSelected = settings.isEnableRainbowSquareBrackets
         doNOTRainbowifyBracketsWithoutContent?.isSelected = settings.isDoNOTRainbowifyBracketsWithoutContent
-        val emptyBorder = EmptyBorder(0, 10, 0, 0)
-        version?.border = emptyBorder
-        version?.text = "Version: ${settings.version}"
-        info?.border = emptyBorder
-        info?.text = """
-            Config file path, for more info see:
-            https://github.com/izhangzhihao/intellij-rainbow-brackets#config-brackets-colors.
-            Path : ${configPath()}
-            """.trimIndent()
-    }
-
-    private fun configPath(): String {
-        val application = ApplicationManager.getApplication()
-        val storageManager = application.stateStore.stateStorageManager
-        val configPath = storageManager.expandMacros("\$APP_CONFIG\$") + "/rainbow_brackets.xml"
-        return Paths.get(configPath).toAbsolutePath().systemIndependentPath
     }
 }
