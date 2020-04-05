77ea05a7459f596b9b14b68aadb40bf0e9162ae8
张志豪 <izhangzhihao@hotmail.com>
2020-04-05 16:13:54 +0800

    Fix usage

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RainbowUpdateNotifyActivity.kt
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RainbowUpdateNotifyActivity.kt
@@ -6,16 +6,34 @@ import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.notification.NotificationListener.UrlOpeningListener
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.util.Disposer
 import org.intellij.lang.annotations.Language
+
 
 class RainbowUpdateNotifyActivity : StartupActivity {
     var updated: Boolean = false
 
     override fun runActivity(project: Project) {
-        RainbowBracketsPluginUpdater.getInstance().runCheckUpdate()
+
+        val eventMulticaster = EditorFactory.getInstance().eventMulticaster
+        val documentListener: DocumentListener = object : DocumentListener {
+            override fun documentChanged(e: DocumentEvent) {
+                val virtualFile = FileDocumentManager.getInstance().getFile(e.document)
+                if (virtualFile != null) {
+                    RainbowBracketsPluginUpdater.getInstance().runCheckUpdate()
+                }
+            }
+        }
+
+        eventMulticaster.addDocumentListener(documentListener, project)
 
         val settings = RainbowSettings.instance
         updated = getPlugin()?.version != settings.version
@@ -24,6 +42,10 @@ class RainbowUpdateNotifyActivity : StartupActivity {
             showUpdate(project)
             updated = false
         }
+
+        Disposer.register(project, Disposable {
+            eventMulticaster.removeDocumentListener(documentListener)
+        })
     }
 
     private fun showUpdate(project: Project) {
