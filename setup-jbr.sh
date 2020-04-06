143763a455f3f42583ac4424d93994c489daa3e1
张志豪 <izhangzhihao@hotmail.com>
2020-04-06 09:08:00 +0800

    Add setup for runtime

---
+++ setup-jbr.sh
@@ -0,0 +1,2 @@
+sudo ln -s /Users/izhangzhihao/Library/Application\ Support/JetBrains/Toolbox/apps/IDEA-U/ch-0/201.6668.60/IntelliJ\ IDEA\ 2020.1\ EAP.app/Contents/jbr /Library/Java/JavaVirtualMachines/jbr-11
+jenv add /Library/Java/JavaVirtualMachines/jbr-11/Contents/Home
