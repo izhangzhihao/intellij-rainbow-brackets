6ee70c6d6655fb8576e665fa1ba78f7bd41a24f7
张志豪 <izhangzhihao@hotmail.com>
2020-09-15 21:48:22 +0800

    Slim clion.jar & add rider.jar & improve support for C# lang(switch to new implementation)

---
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/provider/CSharpBracePairProvider.kt
@@ -0,0 +1,18 @@
+package com.github.izhangzhihao.rainbow.brackets.provider
+
+import com.intellij.lang.BracePair
+import com.jetbrains.rider.ideaInterop.fileTypes.csharp.lexer.CSharpTokenType
+import com.jetbrains.rider.ideaInterop.fileTypes.csharp.lexer.CSharpTokenType.LBRACE
+import com.jetbrains.rider.ideaInterop.fileTypes.csharp.lexer.CSharpTokenType.LBRACKET
+import com.jetbrains.rider.ideaInterop.fileTypes.csharp.lexer.CSharpTokenType.LPARENTH
+import com.jetbrains.rider.ideaInterop.fileTypes.csharp.lexer.CSharpTokenType.RBRACE
+import com.jetbrains.rider.ideaInterop.fileTypes.csharp.lexer.CSharpTokenType.RBRACKET
+import com.jetbrains.rider.ideaInterop.fileTypes.csharp.lexer.CSharpTokenType.RPARENTH
+
+class CSharpBracePairProvider : BracePairProvider {
+    override fun pairs(): List<BracePair> = listOf(
+            BracePair(LPARENTH, RPARENTH, false),
+            BracePair(LBRACE, RBRACE, false),
+            BracePair(LBRACKET, RBRACKET, false)
+    )
+}
