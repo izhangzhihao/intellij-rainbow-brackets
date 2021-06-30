63736b058eb87d85c5d5169533d5006a2623e4d3
张志豪 <izhangzhihao@hotmail.com>
2021-06-30 18:07:22 +0800

    [SQL] the END keyword is improperly detected as closing a BEGIN scope when the END actually closes a CASE #2280

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/provider/SqlBracePairProvider.kt
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/provider/SqlBracePairProvider.kt
@@ -1,9 +1,13 @@
 package com.github.izhangzhihao.rainbow.brackets.provider
 
 import com.intellij.lang.BracePair
+import com.intellij.sql.psi.SqlCommonKeywords.SQL_CASE
 import com.intellij.sql.psi.SqlTokens.SQL_BEGIN
 import com.intellij.sql.psi.SqlTokens.SQL_END
 
 class SqlBracePairProvider : BracePairProvider {
-    override fun pairs(): List<BracePair> = listOf(BracePair(SQL_BEGIN, SQL_END, false))
+    override fun pairs(): List<BracePair> = listOf(
+        BracePair(SQL_BEGIN, SQL_END, false),
+        BracePair(SQL_CASE, SQL_END, false),
+    )
 }
