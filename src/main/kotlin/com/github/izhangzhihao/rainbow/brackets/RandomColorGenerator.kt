909f251152c3878eb3312e58c5a086573599e164
张志豪 <izhangzhihao@hotmail.com>
2021-07-11 14:18:21 +0800

    refactoring: using `jackson` instead of `org.json` since `org.json` not bundled with SDK

--- src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RandomColorGenerator.kt
+++ src/main/kotlin/com/github/izhangzhihao/rainbow/brackets/RandomColorGenerator.kt
@@ -1,22 +1,18 @@
 package com.github.izhangzhihao.rainbow.brackets
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.izhangzhihao.rainbow.brackets.color.Luminosity
 import com.github.izhangzhihao.rainbow.brackets.color.fromString
-import org.json.JSONObject
 import java.awt.Color
 
-fun randomColor(options: String): Color {
-    val options = JSONObject(options)
-    return com.github.izhangzhihao.rainbow.brackets.color.randomColor(
-        fromString(options.getStringOrDefault("hue", "random")),
-        Luminosity.valueOf(options.getStringOrDefault("luminosity", "random"))
-    )
-}
+val mapper: ObjectMapper by lazy { jacksonObjectMapper() }
 
-fun org.json.JSONObject.getStringOrDefault(key: String, default: String): String {
-    return try {
-        this.getString(key)
-    } catch (e: Exception) {
-        default
-    }
+fun randomColor(options: String): Color {
+    val ops: Map<String, String> = mapper.readValue(options)
+    return com.github.izhangzhihao.rainbow.brackets.color.randomColor(
+        fromString(ops.getOrDefault("hue", "random")),
+        Luminosity.valueOf(ops.getOrDefault("luminosity", "random"))
+    )
 }
